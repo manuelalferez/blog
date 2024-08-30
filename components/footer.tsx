dd999dc5c32cd0103c81b32ccbc9b563311aa723
manuelalferez <manuelalferezruiz@gmail.com>
2024-08-30 17:01:43 +0200

    refactor: changed text color footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
     <div className="flex flex-col mt-36">
       <hr className="my-4" />
       <div className="flex flex-col gap-2">
-        <p className="text-sm text-muted-foreground flex gap-1 items-center">
+        <p className="text-sm text-gray-500 flex gap-1 items-center">
           © {currentYear} Made with{" "}
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -22,7 +22,7 @@ export function Footer() {
               d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
             />
           </svg>
-          by <p className="font-medium">Manuel Alférez</p>
+          by Manuel Alférez
         </p>
         <SocialLinks />
       </div>
