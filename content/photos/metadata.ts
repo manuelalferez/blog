2a93f662deb6d1edcc36340b7282b16050f2e4a7
manuelalferez <manuelalferezruiz@gmail.com>
2024-09-25 18:48:16 +0200

    feat: fixing texts of 3 last photos

--- content/photos/metadata.ts
+++ content/photos/metadata.ts
@@ -189,16 +189,16 @@ export const metaData: { [key: string]: PhotoMetaData } = {
   },
   "photo-043.jpg": {
     description:
-      "At the exhibition Andy Warhol & Keith Haring: Party of Life, Ilana is viewing some of the artwork displayed on the screen. The room where it is showcased is stunning, featuring vibrant blues.",
+      "At the Andy Warhol & Keith Haring: Party of Life exhibition, Ilana looks at some of the short films in the exhibition. The room, in which they are exhibited, is captivating as only the screens emit light.",
     date: "2024-09-22",
   },
   "photo-044.jpg": {
-    description: `Notebooks from Orhan Pamuk's exhibitions, exploring the power of objects in storytelling. This collection reflects his experiences with Istanbul's museums and their cultural significance. `,
+    description: `Notebooks of Orhan Pamuk's exhibitions, his notebooks inspired me a lot. The artist writes, paints, crosses out and expresses himself in a very creative way. His whole creative line takes place in the notebooks. He breaks with the conception I had that only in the paintings the artist can express himself, the pages of a notebook are also useful for this.`,
     date: "2024-09-19",
   },
 
   "photo-045.jpg": {
-    description: `Wagen 1, situated within the Verkehrszentrum at the Deutsches Museum in Munich, offers a unique bar experience housed in a beautifully refurbished vintage train. `,
+    description: `At random places in Munich you can find old wagons, which have been adapted and given a new life. Social life makes its way through them.`,
     date: "2024-09-17",
   },
 };
