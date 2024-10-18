42dfb83c43e8eddf8f2b3c3689f69a9e5559dbd1
manuelalferez <manuel.secondary@icloud.com>
2024-10-18 17:00:01 +0200

    fix: error scaping apos

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="prose dark:prose-invert">
       <p className="mt-0 text-slate-700 dark:text-slate-200">
-        Hi, this is my little corner on the Internet. I'm Manuel, Front-End
+        Hi, this is my little corner on the Internet. I&apos;m Manuel, Front-End
         Engineer based in Spain, passionate about event organization, previously
         Lead Organizer{" "}
         <CustomLink href="https://www.meetup.com/es-ES/gdg-jaen/">
